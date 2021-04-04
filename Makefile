882413e54ea2aaa617deddd3316686f630c1ad23
achiyazigi <matanelcohen0@walla.com>
2021-04-04 10:41:14 +0000

    in the middle of 2_1

--- Makefile
+++ Makefile
@@ -8,9 +8,12 @@ main1_1: main1_1.c hello_ariel.c hello_ariel.h
 	$(CC) $^ -o $@
 main1_2: main1_2.c libhello_ariel.so
 	$(CC) -L. main1_2.c -lhello_ariel -o $@
-	export LD_LIBRARY_PATH=.
+	export LD_LIBRARY_PATH=.; ./main1_2
 main1_3: main1_3.c libhello_ariel.so
 	$(CC) $< -fPIC -ldl -o $@
+main2_1: main2_1.c
+	$(CC) $< -o $@
+	pstree ./main2_1
 hello_ariel.o: hello_ariel.c hello_ariel.h
 	$(CC) -c -fPIC $<
 libhello_ariel.so: hello_ariel.o
--- main2_1.c
+++ main2_1.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(){
-	
 	int i = 1;
+	int fork_ans;
 	printf("pid: %d\n", getpid());
-	return 0;
+	if((fork_ans = fork())== -1){
+		printf("fork failed\n");
+	}
+	else if(fork_ans == 0){
+		printf("im the child\n");
+			int fork_ans2;
+		if((fork_ans2 = fork())== -1){
+			printf("fork failed\n");
+		}
+		else if(fork_ans2 == 0){
+			printf("im the grandson\n");
+		}
+	}
+
+	else if(fork_ans > 0){
+		printf("im the father, my child id is: %d\n", fork_ans);
+	}
+	exit(0);
 }
